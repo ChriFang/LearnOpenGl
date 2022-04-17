e023efa677faad068ebde6486c94153539859ed1
ChriFang <437385323@qq.com>
2022-04-17 21:38:21 +0800

    MyPattern

--- LearnOpenGL/LearnOpenGL/triangle.cpp
+++ LearnOpenGL/LearnOpenGL/triangle.cpp
@@ -147,7 +147,8 @@ int main()
     // draw our first triangle
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
     // glBindVertexArray(0); // no need to unbind it every time 
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
